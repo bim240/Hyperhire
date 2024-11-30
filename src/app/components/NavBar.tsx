23f885d6ad264cac2b7b6ada76655e0482b75dca
bim240 <bimlendu357@gmail.com>
2024-11-30 14:22:22 +0530

    add header navigation

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,36 @@
+'use client'
+import React from 'react'
+import navIcon from '@/app/assets/logo.svg'
+import Image from 'next/image'
+import Select, {Options} from './Select'
+import {Button} from '@headlessui/react'
+
+export default function NavBar() {
+  const options: Options[] = [
+    {id: 1, name: '채용'},
+    {id: 2, name: '해외 개발자 원격 채용'},
+    {id: 3, name: '외국인 원격 채용 (비개발 직군)'},
+    {id: 4, name: '한국어 가능 외국인 채용'},
+  ]
+  return (
+    <nav className="bg-transparent">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between min-w-full">
+          <div className="">
+            <Image src={navIcon} alt="nav" />
+          </div>
+          <div className="hidden flex items-center justify-center gap-5 sm:flex">
+            <Select options={options} />
+            <p className="text-white font-extrabold"> 해외 개발자 활용 서비스</p>
+          </div>
+          <div className="hidden flex items-center pr-2 sm:flex">
+            <Button className="bg-white text-blue-700 font-extrabold px-4 py-2 rounded">
+              {' '}
+              문의하기
+            </Button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
---
+++ src/app/components/Select.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import {useState} from 'react'
+import {Label, Listbox, ListboxButton, ListboxOption, ListboxOptions} from '@headlessui/react'
+import {ChevronDownIcon} from '@heroicons/react/16/solid'
+import {CheckIcon} from '@heroicons/react/20/solid'
+
+export interface Options {
+  id: string | number
+  name: string
+}
+
+export default function Select({label, options}: {label?: string; options: Options[]}) {
+  const [selected, setSelected] = useState(options[0])
+
+  return (
+    <Listbox value={selected} onChange={setSelected}>
+      {label && <Label className="block text-sm/6 font-medium text-gray-900">Assigned to</Label>}
+      <div className="relative mt-2">
+        <ListboxButton className="grid w-full w-36  cursor-pointer grid-cols-1 rounded-md bg-transparent py-1.5 pl-3 pr-2 text-left text-gray-900 bg-transparent sm:text-sm/6">
+          <p className="col-start-1 row-start-1 flex items-center gap-3 pr-6">
+            <p className="block truncate text-white font-extrabold">{selected.name}</p>
+          </p>
+          <ChevronDownIcon
+            aria-hidden="true"
+            className="col-start-1 row-start-1 size-5 self-center justify-self-end text-gray-500 sm:size-4 text-white text-lg"
+          />
+        </ListboxButton>
+
+        <ListboxOptions
+          transition
+          className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in sm:text-sm"
+        >
+          {options.map(option => (
+            <ListboxOption
+              key={option.id}
+              value={option}
+              className="group relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white data-[focus]:outline-none"
+            >
+              <div className="flex items-center">
+                <span className="ml-3 block truncate font-normal group-data-[selected]:font-semibold">
+                  {option.name}
+                </span>
+              </div>
+
+              <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden">
+                <CheckIcon aria-hidden="true" className="size-5" />
+              </span>
+            </ListboxOption>
+          ))}
+        </ListboxOptions>
+      </div>
+    </Listbox>
+  )
+}
